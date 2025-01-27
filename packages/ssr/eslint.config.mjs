31a6effec67bdc527c2835ccf725db0b16be0866
GLY <dc_langx@hotmail.com>
2025-01-27 10:22:21 +0800

    chore: 更新依赖

--- packages/ssr/eslint.config.mjs
+++ packages/ssr/eslint.config.mjs
@@ -1,7 +1,7 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginPrettierRecommendedConfigs from "eslint-plugin-prettier/recommended";
-
+import eslintPluginJsonc from "eslint-plugin-jsonc";
 export default tseslint.config(
 	{
 		// config with just ignores is the replacement for `.eslintignore`
@@ -15,6 +15,9 @@ export default tseslint.config(
 
 	// strict: a superset of recommended that includes more opinionated rules which may also catch bugs.
 	// ...tseslint.configs.strict,
+
+	// recommended Json config
+	...eslintPluginJsonc.configs["flat/prettier"],
 
 	// // stylistic: additional rules that enforce consistent styling without significantly catching bugs or changing logic.
 	// ...tseslint.configs.stylistic,
